50101a7c43efbfcb62d57562adfeb01c4325b129
Richard Parsons <parsons.rk@gmail.com>
2021-06-12 16:33:35 +0200

    Refactor RM Animation

--- src/useRMAnimation.ts
+++ src/useRMAnimation.ts
@@ -1,26 +1,27 @@
-import { Vector3 } from '@babylonjs/core'
+import { AnimationGroup, Vector3 } from '@babylonjs/core'
+
 import { useScene } from 'react-babylonjs'
 
 function useRMAnimation(animationName: string) {
     const scene = useScene()
 
+    const reset = (animationGroup: AnimationGroup, valueOffset: Vector3) => {
+        const newKeys = animationGroup.targetedAnimations[0].animation
+            .getKeys()
+            .map((x) => ({ ...x, value: (x.value as Vector3).add(valueOffset) }))
+        animationGroup.targetedAnimations[0].animation.setKeys(newKeys)
+        animationGroup?.play(false)
+    }
+
     const init = () => {
         const animationGroup = scene?.getAnimationGroupByName(animationName)
 
-        const animationOffset = animationGroup?.targetedAnimations[0].animation
-            .getKeys()
-            .slice(-1)[0].value as Vector3
+        const valueOffset = animationGroup?.targetedAnimations[0].animation.getKeys().slice(-1)[0]
+            .value as Vector3
 
         animationGroup?.play(false)
-        animationGroup?.setWeightForAllAnimatables(1)
 
-        animationGroup?.onAnimationGroupEndObservable.add(() => {
-            const newKeys = animationGroup.targetedAnimations[0].animation
-                .getKeys()
-                .map((x) => ({ ...x, value: (x.value as Vector3).add(animationOffset) }))
-            animationGroup.targetedAnimations[0].animation.setKeys(newKeys)
-            animationGroup?.play(false)
-        })
+        animationGroup?.onAnimationGroupEndObservable.add(() => reset(animationGroup, valueOffset))
     }
 
     const render = () => {
