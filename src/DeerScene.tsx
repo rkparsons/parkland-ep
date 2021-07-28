6f7f880e38827d7e4c5a3fcff8120a7088ca676d
Rick Parsons <parsons.rk@gmail.com>
2021-07-28 13:26:04 +0200

    Remove comments

--- src/DeerScene.tsx
+++ src/DeerScene.tsx
@@ -13,29 +13,19 @@ const DeerWithPointAndClickControls = withPointAndClickControls(withWaypointCont
 // todo: move tweakable params to .env
 const DeerScene: FC = () => (
     <Scene>
-        <hemisphericLight name="light1" direction={new Vector3(0, 1, 0)} intensity={0.1} />
-        <arcRotateCamera
-            name="camera1"
-            alpha={Math.PI}
-            beta={Math.PI / 2}
-            radius={20.0}
-            target={Vector3.Zero()}
-            minZ={0.001}
-        />
-        <SkyAnimated />
-        {/* <GroundProvider GroundComponent={Ground}>
+        <GroundProvider GroundComponent={Ground}>
             <arcRotateCamera
                 name="camera1"
-                alpha={Math.PI / 2}
-                beta={Math.PI / 2.5}
-                radius={9.0}
-                target={new Vector3(0, 2, 2)}
+                alpha={Math.PI}
+                beta={Math.PI / 2.3}
+                radius={20.0}
+                target={Vector3.Zero()}
                 minZ={0.001}
             />
             <hemisphericLight name="hemi-light" intensity={0.7} direction={Vector3.Up()} />
             <SkyAnimated />
             <DeerWithPointAndClickControls />
-        </GroundProvider> */}
+        </GroundProvider>
     </Scene>
 )
 
