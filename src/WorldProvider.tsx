4669a6f023e03f4cf47232fc7acc128fbbcea81d
localist-richardparsons <localist@richardparsons.de>
2022-01-26 13:12:09 +0100

    Hover meshes

--- src/WorldProvider.tsx
+++ src/WorldProvider.tsx
@@ -1,16 +1,6 @@
-import {
-    AbstractMesh,
-    ActionManager,
-    Color3,
-    GlowLayer,
-    HighlightLayer,
-    InterpolateValueAction,
-    Mesh,
-    SetValueAction,
-    Vector3
-} from '@babylonjs/core'
+import { AbstractMesh, ActionManager, ExecuteCodeAction, Mesh, Vector3 } from '@babylonjs/core'
 import { FC, ReactNode, Suspense, useEffect, useRef, useState } from 'react'
-import { ILoadedModel, Model, useHover, useScene } from 'react-babylonjs'
+import { ILoadedModel, Model, useBeforeRender, useHover, useScene } from 'react-babylonjs'
 
 import GroundContext from './WorldContext'
 
@@ -22,40 +12,42 @@ const WorldProvider: FC<ViewProps> = ({ children }) => {
     const world = useRef<AbstractMesh>()
     const octahedron = useRef<Mesh | null>(null)
     const scene = useScene()
-    const highlightLayer = useRef<HighlightLayer>()
 
     const onModelLoaded = (loadedModel: ILoadedModel) => {
         world.current = loadedModel.meshes?.find((x) => x.name === 'Planet Top')
+        loadedModel.meshes?.forEach(({ name }) => console.log(name))
         octahedron.current = loadedModel.meshes?.find((x) => x.name === 'octahedron ') as Mesh
-
-        highlightLayer.current = new HighlightLayer('octahedronHighlight', scene!)
-        highlightLayer.current.isEnabled = false
-        highlightLayer.current.addMesh(
-            octahedron.current,
-            new Color3(162 / 255, 140 / 255, 147 / 255)
-        )
-        highlightLayer.current.blurHorizontalSize = 5
-        highlightLayer.current.blurVerticalSize = 5
-
-        octahedron.current.actionManager = new ActionManager(scene!)
-        octahedron.current.actionManager.registerAction(
-            new SetValueAction(
-                ActionManager.OnPointerOverTrigger,
-                highlightLayer.current,
-                'isEnabled',
-                true
+        loadedModel.meshes
+            ?.filter(({ name }) =>
+                [
+                    'Cube with peaks',
+                    'Pokemon',
+                    'Cross',
+                    'Anvil',
+                    'TCube',
+                    'Double tetrahedron',
+                    'dodecahedron',
+                    'Star',
+                    'octahedron ',
+                    'Rock',
+                    'wheel',
+                    'Classic Solid',
+                    'Solid',
+                    'FO1',
+                    'FO2',
+                    'FO3'
+                ].includes(name)
             )
-        )
-        octahedron.current.actionManager.registerAction(
-            new SetValueAction(
-                ActionManager.OnPointerOutTrigger,
-                highlightLayer.current,
-                'isEnabled',
-                false
-            )
-        )
+            .forEach((mesh) => {
+                mesh.actionManager = new ActionManager(scene!)
+                mesh.actionManager.registerAction(
+                    new ExecuteCodeAction(
+                        ActionManager.OnPointerOverTrigger,
+                        () => (scene!.hoverCursor = 'pointer')
+                    )
+                )
+            })
     }
-
     return (
         <GroundContext.Provider value={{ world }}>
             {children}
@@ -67,15 +59,6 @@ const WorldProvider: FC<ViewProps> = ({ children }) => {
                     rootUrl={`${process.env.PUBLIC_URL}/`}
                     sceneFilename="World.glb"
                     onModelLoaded={onModelLoaded}
-                />
-            </Suspense>
-            <Suspense fallback={null}>
-                <Model
-                    name="satellite"
-                    position={new Vector3(0, 93, 10)}
-                    scaling={new Vector3(10, 10, 10)}
-                    rootUrl={`${process.env.PUBLIC_URL}/`}
-                    sceneFilename="Satellite.glb"
                 />
             </Suspense>
         </GroundContext.Provider>
