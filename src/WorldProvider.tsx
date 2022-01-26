8cb222df34351456a9afa8ecad38a1a8a8eb53cf
localist-richardparsons <localist@richardparsons.de>
2022-01-26 12:06:51 +0100

    Add highlight

--- src/WorldProvider.tsx
+++ src/WorldProvider.tsx
@@ -1,6 +1,16 @@
-import { AbstractMesh, Vector3 } from '@babylonjs/core'
-import { FC, ReactNode, Suspense, useRef } from 'react'
-import { ILoadedModel, Model, useScene } from 'react-babylonjs'
+import {
+    AbstractMesh,
+    ActionManager,
+    Color3,
+    GlowLayer,
+    HighlightLayer,
+    InterpolateValueAction,
+    Mesh,
+    SetValueAction,
+    Vector3
+} from '@babylonjs/core'
+import { FC, ReactNode, Suspense, useEffect, useRef, useState } from 'react'
+import { ILoadedModel, Model, useHover, useScene } from 'react-babylonjs'
 
 import GroundContext from './WorldContext'
 
@@ -10,16 +20,34 @@ type ViewProps = {
 
 const WorldProvider: FC<ViewProps> = ({ children }) => {
     const world = useRef<AbstractMesh>()
+    const octahedron = useRef<Mesh | null>(null)
+    const scene = useScene()
+    const highlightLayer = useRef<HighlightLayer>(null)
 
     const onModelLoaded = (loadedModel: ILoadedModel) => {
         world.current = loadedModel.meshes?.find((x) => x.name === 'Planet Top')
-
-        loadedModel.meshes?.forEach((x) => console.log(x.name))
+        octahedron.current = loadedModel.meshes?.find((x) => x.name === 'octahedron ') as Mesh
+        highlightLayer.current!.addMesh(octahedron.current, Color3.White())
     }
+
+    useHover(
+        () => {
+            if (highlightLayer.current) {
+                highlightLayer.current.isEnabled = true
+            }
+        },
+        () => {
+            if (highlightLayer.current) {
+                highlightLayer.current.isEnabled = false
+            }
+        },
+        octahedron
+    )
 
     return (
         <GroundContext.Provider value={{ world }}>
             {children}
+            <highlightLayer name="hl" ref={highlightLayer} />
             <Suspense fallback={null}>
                 <Model
                     name="world"
