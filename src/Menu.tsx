0dc83d18a47cc13cd1e2480f0fe9abf16976b61f
localist-richardparsons <localist@richardparsons.de>
2022-01-28 14:31:01 +0100

    Menu styling

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -40,7 +40,8 @@ const Menu: FC<ViewProps> = ({ audioLoops }) => {
             <div className="Footer">&copy; SINE LANGUAGE RECORDS</div>
             {isActive && (
                 <div className="SoundWarning">
-                    {/* <svg
+                    <svg
+                        className="SoundWarningIcon"
                         xmlns="http://www.w3.org/2000/svg"
                         enableBackground="new 0 0 24 24"
                         height="24px"
@@ -54,11 +55,8 @@ const Menu: FC<ViewProps> = ({ audioLoops }) => {
                         <g>
                             <path d="M12,3c-4.97,0-9,4.03-9,9v7c0,1.1,0.9,2,2,2h4v-8H5v-1c0-3.87,3.13-7,7-7s7,3.13,7,7v1h-4v8h4c1.1,0,2-0.9,2-2v-7 C21,7.03,16.97,3,12,3z M7,15v4H5v-4H7z M19,19h-2v-4h2V19z" />
                         </g>
-                    </svg> */}
-                    <span role="img" aria-label="headphones">
-                        &#127911;
-                    </span>{' '}
-                    SOUND ON
+                    </svg>
+                    <span className="SoundWarningText">SOUND ON</span>
                 </div>
             )}
         </div>
