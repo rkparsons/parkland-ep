950933b94d786829d1bc990850f91a5348b5e1bc
localist-richardparsons <localist@richardparsons.de>
2022-01-29 14:50:09 +0100

    Add subtitles fade

--- src/Subtitles.tsx
+++ src/Subtitles.tsx
@@ -1,13 +1,22 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 type ViewProps = {
     subtitles: string
 }
 
 const Subtitles: FC<ViewProps> = ({ subtitles }) => {
+    const [text, setText] = useState('')
+    const isVisible = subtitles !== ''
+
+    useEffect(() => {
+        if (subtitles) {
+            setText(subtitles)
+        }
+    }, [subtitles])
+
     return (
-        <div className="SubtitlesContainer">
-            <span className="SubtitlesText">{subtitles}</span>
+        <div className={`SubtitlesContainer ${!isVisible && 'FadeOut'}`}>
+            <span className="SubtitlesText">{text}</span>
         </div>
     )
 }
