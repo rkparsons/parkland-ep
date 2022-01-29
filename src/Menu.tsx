6dc56ab097ac806236250bd84210a653a57cdaef
localist-richardparsons <localist@richardparsons.de>
2022-01-29 18:28:22 +0100

    Styling fixes

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -36,29 +36,33 @@ const Menu: FC<ViewProps> = ({ audioLoops }) => {
         <div role="button" tabIndex={0} className="Menu" onClick={closeMenu} onKeyDown={closeMenu}>
             <div className={`Overlay ${isActive ? '' : 'FadeOut'}`}>
                 <div className={`Backdrop ${isActive ? '' : 'IgnoreClick'}`} />
-                {isActive && <p className="StartText">Click to start</p>}
             </div>
-            <div className="Footer">&copy; 2022 SINE LANGUAGE RECORDS</div>
+            <div className="Footer">
+                <b>&copy; 2022 SINE LANGUAGE RECORDS</b>
+            </div>
             {isActive && (
-                <div className="SoundWarning">
-                    <svg
-                        className="SoundWarningIcon"
-                        xmlns="http://www.w3.org/2000/svg"
-                        enableBackground="new 0 0 24 24"
-                        height="24px"
-                        viewBox="0 0 24 24"
-                        width="24px"
-                        fill="#000000"
-                    >
-                        <g>
-                            <rect fill="none" height="24" width="24" />
-                        </g>
-                        <g>
-                            <path d="M12,3c-4.97,0-9,4.03-9,9v7c0,1.1,0.9,2,2,2h4v-8H5v-1c0-3.87,3.13-7,7-7s7,3.13,7,7v1h-4v8h4c1.1,0,2-0.9,2-2v-7 C21,7.03,16.97,3,12,3z M7,15v4H5v-4H7z M19,19h-2v-4h2V19z" />
-                        </g>
-                    </svg>
-                    <span className="SoundWarningText">SOUND ON</span>
-                </div>
+                <>
+                    <p className="StartText">Click to start</p>
+                    <div className="SoundWarning">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            enableBackground="new 0 0 24 24"
+                            height="24px"
+                            viewBox="0 0 24 24"
+                            width="24px"
+                        >
+                            <g>
+                                <rect fill="none" height="24" width="24" />
+                            </g>
+                            <g>
+                                <path d="M12,3c-4.97,0-9,4.03-9,9v7c0,1.1,0.9,2,2,2h4v-8H5v-1c0-3.87,3.13-7,7-7s7,3.13,7,7v1h-4v8h4c1.1,0,2-0.9,2-2v-7 C21,7.03,16.97,3,12,3z" />
+                            </g>
+                        </svg>
+                        <span className="SoundWarningText">
+                            <b>SOUND ON</b>
+                        </span>
+                    </div>
+                </>
             )}
             {!isActive && (
                 <>
