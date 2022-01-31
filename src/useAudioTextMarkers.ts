a23153385f74f826b2dc94747ef1e9444a394603
localist-richardparsons <localist@richardparsons.de>
2022-01-31 17:12:26 +0100

    Remove collisions

--- src/useAudioTextMarkers.ts
+++ src/useAudioTextMarkers.ts
@@ -39,8 +39,8 @@ const useAudioTextMarkers = (setSubtitles: (subtitles: string) => void) => {
     ) {
         const mesh = worldModel.meshes!.find(({ name }) => name === subtitleName)!
         mesh.isPickable = false
-        mesh.isVisible = true
-        mesh.checkCollisions = true
+        mesh.isVisible = false
+        mesh.checkCollisions = false
         meshes.current.push(mesh)
 
         if (subtitleText) {
